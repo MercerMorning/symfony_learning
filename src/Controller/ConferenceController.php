a42fd18c4a89a7759968398638ba08ea45a63a0d
raketa_nickolay <hugopochta@gmail.com>
2021-07-12 23:44:32 +0300

    'added_controller'

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(Request $request): Response
+    {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        return new Response(<<<EOF
+            <html>
+             $greet
+                <body>
+                   <img src="/images/under-construction.gif" />
+                </body>
+            </html>
+EOF
+        );
+    }
+}
