5e5ec963eb89a93386ae9086894821cef8b2717f
raketa_nickolay <hugopochta@gmail.com>
2021-07-13 00:36:58 +0300

    'commit'

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -10,12 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/hello/{name}", name="homepage")
      */
-    public function index(Request $request): Response
+    public function index(string $name = ''): Response
     {
         $greet = '';
-        if ($name = $request->query->get('hello')) {
+        if ($name) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
         return new Response(<<<EOF
